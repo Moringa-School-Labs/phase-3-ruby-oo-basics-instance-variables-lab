2626007970a02c9303fdf914aaf91eafb274c71d
catherine <vuthi.esther@gmail.com>
2022-06-09 20:38:26 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
 
+    #setter method - assigns value to a variable
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    #Getter method - Responsible for  reporting the name
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
